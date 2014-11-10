b2fbc18c9db1df96a37e306d1b8e9fe214ee83c7
Klaha <klaha.77@gmail.com>
2014-11-10 15:33:31 -0430

    Initial Commit

---
+++ lib-jm.js
@@ -0,0 +1,60 @@
+var caja = ["Juan", "Jose", "Jonathan", "Chiabe"];
+var ciclo = true;
+var i;
+
+function hack_delete(posicion, arreglo)
+{
+  var nuevaCaja = [];
+
+  for (i = 0; i < arreglo.length; i++)
+  {
+    if ( i != posicion )
+    {
+      nuevaCaja.push(arreglo[i]);
+    }
+  }
+  return nuevaCaja;
+}
+
+// console.log(hack_delete(2, caja));
+
+function hack_pop(arreglo)
+{
+  var nuevaCaja2 = [];
+
+  for (i = 0; i < arreglo.length; i++)
+  {
+    if ( i !== 0 )
+    {
+      nuevaCaja2.push(arreglo[i]);
+    }
+  }
+  return [arreglo[0], nuevaCaja2];
+}
+
+// console.log(hack_pop(caja));
+
+function hack_push(elemento, arreglo)
+{
+  arreglo[arreglo.length] = elemento;
+  return arreglo;
+}
+
+// console.log(hack_push("Obama", caja))
+
+function hack_search(elemento, arreglo)
+{
+  var pos = -1;
+
+  for (i = 0; i < arreglo.length; i++)
+  {
+    if (arreglo[i] === elemento)
+    {
+      pos = i;
+    }
+  }
+  return pos;
+}
+
+// console.log(hack_search('Jonathan', caja));
+// console.log(hack_search('Hack', caja));
